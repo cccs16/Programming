54be271b65c1440a18c9365a76d30e7c64fde921
Caleb <75612210+cccs16@users.noreply.github.com>
2024-05-31 03:42:21 -0400

    Delete Python directory

--- Python/1.py
+++
@@ -1,6 +0,0 @@
-bicycles = ['trek', 'cannondale', 'redline', 'specialized']
-print(bicycles[-1].title())
-bicycles[3] = 'unicycle'
-print(bicycles[-1].title())
-bicycles.append('specialized')
-print(bicycles[4])
